eefcb0b3d3f57da0593d5d4d4e287203518e7f98
VeamVeat <vv1629728@gmail.com>
2021-09-29 23:44:12 +0300

    feat: added task async.py

---
+++ python_fundamentals/async.py
@@ -0,0 +1,35 @@
+import requests as r
+import asyncio
+
+
+async def get_images_cat():
+
+    header = {'x-api-key': 'bd847905-bc01-4650-ab52- ace3247ee273'}
+    img = r.get('https://api.thecatapi.com/v1/images/search', headers=header)
+    img_url: str = img.json()[0]["url"]
+
+    img_file = img_url.split('/')[-1]
+    img_name = img_file[:-4]
+    img_expansion = img_file[-3:]
+
+    img = r.get(f'{img_url}', headers=header)
+
+    out = open(f"pictures_of_cats/{img_name}.{img_expansion}", "wb")
+    out.write(img.content)
+    out.close()
+
+
+async def main(count_task: int):
+
+    task = [loop.create_task(get_images_cat()) for _ in range(count_task)]
+    await asyncio.wait(task)
+
+
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(2))
+
+    except:
+        pass
+
