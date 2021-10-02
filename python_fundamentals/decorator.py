c15252c63be7d1aaf916c5a333bb1011825d3c75
VeamVeat <vv1629728@gmail.com>
2021-10-02 22:12:45 +0300

    feat: added class decorator Timer

--- python_fundamentals/decorator.py
+++ python_fundamentals/decorator.py
@@ -1,23 +1,25 @@
-from datetime import datetime
 from math import sin, cos
 
 
-def timeit(func):
+class Timer:
+    from time import time
 
-    def wrapper(*args, **kwargs):
-        start = datetime.now()
-        result = func(*args, **kwargs)
-        end = datetime.now()
-        print(f" Время начала работы функции: {start}\n Время окончания работы функции: {end}\n"
-              f" Общее время работы функции: {end - start}\n")
+    def __init__(self, fun):
+        self.function = fun
+
+    def __call__(self, *args, **kwargs):
+        start_time = self.time()
+        result = self.function(*args, **kwargs)
+        end_time = self.time()
+        print(f" Время начала работы функции: {start_time}\n Время окончания работы функции: {end_time}\n"
+              f" Общее время работы функции: {end_time - start_time}\n")
         return result
-    return wrapper
 
 
-@timeit
+@Timer
 def calculate_mathematical_expression(number_x: int, number_y: int) -> float:
     return (cos(number_x) * sin(number_y))**sin(number_x + number_y)
 
 
 result_func = calculate_mathematical_expression(3, 4)
-print(result_func)
+print(' Результат функции: ', result_func)
