f7542858ed08afecaef0dda22880735e69525d80
VeamVeat <vv1629728@gmail.com>
2021-10-03 22:14:52 +0300

    feat: added task class_inheritance.py

---
+++ python_fundamentals/сlass_Inheritance.py
@@ -0,0 +1,43 @@
+class First:
+    def get_name(self):
+        return f'{self.__class__.__name__}'
+
+    @staticmethod
+    def get_type():
+        return 'Class'
+
+
+class Second(First):
+    @classmethod
+    def get_name(cls):
+        return f'{cls.__class__.__name__}'
+
+
+class Third:
+    def get_programming_language(self):
+        return "Python"
+
+
+class GeneralOne(Second, Third, First):
+    pass
+
+
+class GeneralTwo(Second, Third, First):
+    def get_programming_language(self):
+        super().get_programming_language()
+        return "Python-love"
+
+
+class GeneralThree(Second, Third, First):
+    pass
+
+
+a = GeneralOne()
+print(a.get_name())
+
+b = GeneralTwo()
+print(b.get_programming_language())
+
+c = GeneralThree()
+print(c.get_type())
+print(GeneralThree.get_type())
