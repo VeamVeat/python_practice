e0bf3bff5bac28eba13d8c6ce11b5f971fb5cde6
VeamVeat <vv1629728@gmail.com>
2021-10-04 18:13:01 +0300

    feat: create task class_inheritance.py

--- python_fundamentals/async.py
+++ python_fundamentals/async.py
@@ -2,34 +2,32 @@ import requests as r
 import asyncio
 
 
-async def get_images_cat():
+async def get_images_cat(img_info):
+    img_info = await img_info
 
-    header = {'x-api-key': 'bd847905-bc01-4650-ab52- ace3247ee273'}
-    img_info = r.get('https://api.thecatapi.com/v1/images/search', headers=header)
-    img_url: str = img_info.json()[0]["url"]
+    if img_info.status_code == 200:
+        img_url: str = img_info.json()[0]["url"]
 
-    img_file = img_url.split('/')[-1]
-    img_name = img_file[:-4]
-    img_expansion = img_file[-3:]
+        img_file = img_url.split('/')[-1]
+        img_name = img_file[:-4]
+        img_expansion = img_file[-3:]
 
-    img = r.get(f'{img_url}', headers=header)
+        img = r.get(f'{img_url}')
 
-    out = open(f"Cats/{img_name}.{img_expansion}", "wb")
-    out.write(img.content)
-    out.close()
+        with open(f"Cats/{img_name}.{img_expansion}", "wb") as o:
+            o.write(img.content)
 
 
 async def main(count_task: int):
 
-    task = [loop.create_task(get_images_cat()) for _ in range(count_task)]
-    await asyncio.wait(task)
+    task = [loop.create_task(get_images_cat(loop.run_in_executor(None, r.get, f'https://api.thecatapi.com/v1/images/search'))) for _ in range(count_task)]
+    await asyncio.gather(*task)
 
 
 if __name__ == "__main__":
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(main(2))
+        loop.run_until_complete(main(5))
 
     except:
         pass
-
