480a332017a772c36d13a0fa053ba9b03d23e936
VeamVeat <vv1629728@gmail.com>
2021-10-05 13:34:29 +0300

    feat: fix error in task async.py

--- python_fundamentals/async.py
+++ python_fundamentals/async.py
@@ -8,19 +8,21 @@ async def get_images_cat(img_info):
     if img_info.status_code == 200:
         img_url: str = img_info.json()[0]["url"]
 
-        img_file = img_url.split('/')[-1]
-        img_name = img_file[:-4]
-        img_expansion = img_file[-3:]
+        img_name = img_url.split('/')[-1]
 
-        img = r.get(f'{img_url}')
+        img_content = r.get(img_url).content
 
-        with open(f"Cats/{img_name}.{img_expansion}", "wb") as o:
-            o.write(img.content)
+        with open(f"Cats/{img_name}", "wb") as file:
+            file.write(img_content)
 
 
 async def main(count_task: int):
 
-    task = [loop.create_task(get_images_cat(loop.run_in_executor(None, r.get, f'https://api.thecatapi.com/v1/images/search'))) for _ in range(count_task)]
+    task = [loop.create_task(get_images_cat(loop.run_in_executor(None,
+                                                                 r.get,
+                                                                 f'https://api.thecatapi.com/v1/images/search')))
+            for _ in range(count_task)]
+
     await asyncio.gather(*task)
 
 
