e4fe8bd481301b9832be68867e2533670c6f6d12
VeamVeat <vv1629728@gmail.com>
2021-10-05 17:52:16 +0300

    feat: fix error in task max_long_period_of_time.py

--- python_fundamentals/max_long_period_of_time.py
+++ python_fundamentals/max_long_period_of_time.py
@@ -8,5 +8,4 @@ def longest_time(h: int, m: int, s: int) -> str:
     return max(time_hour_minute_second, key=time_hour_minute_second.get)
 
 
-t = longest_time(15, 955, 59400)
-print("Максимально длительный промежуток времени -> ", t)
+print("Максимально длительный промежуток времени -> ", longest_time(15, 955, 59400))
