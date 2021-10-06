1604d448eab9036c7319be381deaca5a030e10db
VeamVeat <vv1629728@gmail.com>
2021-10-06 11:20:27 +0300

    feat: fix error in task string_to_int.py

--- python_fundamentals/string_to_Int.py
+++ python_fundamentals/string_to_Int.py
@@ -1,24 +1,31 @@
-class DataValidException(Exception):
-    pass
+import re
 
 
-def str_to_int(string: str) -> int:
-    if string.isdecimal():
+def str_to_int(user_string: str) -> int:
+
+    num_format = re.compile(r'^\-?[1-9][0-9]*\.?[0-9]*')
+    is_number = re.match(num_format, user_string)
+
+    if is_number:
         str_number = [f'{i}' for i in range(10)]
         result_list = []
-        for id_i, i in enumerate(string):
-            for id_j, j in enumerate(str_number):
-                if j == i and id_i == len(string) - 1:
-                    result_list.append(id_j)
-                elif j == i:
-                    result_list.append(id_j * 10 ** (len(string) - 1 - id_i))
+
+        for user_index_in_string, user_symbol_in_string in enumerate(user_string):
+            for index_in_str_number, symbol_in_str_number in enumerate(str_number):
+
+                if symbol_in_str_number == user_symbol_in_string and user_index_in_string == len(user_string) - 1:
+                    result_list.append(index_in_str_number)
+
+                elif symbol_in_str_number == user_symbol_in_string:
+                    result_list.append(index_in_str_number * 10 ** (len(user_string) - 1 - user_index_in_string))
 
     else:
-        raise DataValidException("Строка " + string + " не является валидной!")
-    return sum(result_list)
+        raise Exception("Строка " + user_string + " не является валидной!")
+    return sum(result_list) if user_string[0] != '-' else sum(result_list) * -1
 
 
-s_input = input()
-print('Введённое число:', s_input, '->', type(s_input))
-s = str_to_int(s_input)
-print('Преобразованное число:', s, '->', type(s))
+string_input = input()
+print('Введённое число:', string_input, '->', type(string_input))
+user_string_input = str_to_int(string_input)
+print('Преобразованное число:', user_string_input, '->', type(user_string_input))
+
