2232b56b3f78e52c1773c1d40688b1dd95feb7dd
VeamVeat <vv1629728@gmail.com>
2021-10-07 14:21:57 +0300

    fix: fix error in task async.py

--- python_fundamentals/async.py
+++ python_fundamentals/async.py
@@ -2,24 +2,29 @@ import requests as r
 import asyncio
 
 
-async def get_images_cat(img_future):
+async def save_image_cat(img_content_response, img_name):
+    print("Сохранение изображения")
+
+    with open(f"Cats/{img_name}", "wb") as file:
+        file.write((await img_content_response).content)
+
+
+async def get_image_cat(img_future):
     img_future = await img_future
+    print(img_future, "Получение изображения")
 
     if img_future.status_code == 200:
-
         img_url: str = img_future.json()[0]["url"]
         img_name = img_url.split('/')[-1]
-        img_content = r.get(img_url).content
 
-        with open(f"Cats/{img_name}", "wb") as file:
-            file.write(img_content)
+        await save_image_cat(loop.run_in_executor(None, r.get, img_url), img_name)
 
 
 async def main(count_task: int):
 
-    task = [loop.create_task(get_images_cat(loop.run_in_executor(None,
-                                                                 r.get,
-                                                                 f'https://api.thecatapi.com/v1/images/search')))
+    info_img_response = f'https://api.thecatapi.com/v1/images/search'
+
+    task = [loop.create_task(get_image_cat(loop.run_in_executor(None, r.get, info_img_response)))
             for _ in range(count_task)]
 
     await asyncio.gather(*task)
