7e4af1f7f3a781c54da960102712aec40bf4d76d
VeamVeat <vv1629728@gmail.com>
2021-10-18 13:49:27 +0300

    feat: written decorators as a function

--- python_fundamentals/decorator.py
+++ python_fundamentals/decorator.py
@@ -2,21 +2,19 @@ from math import sin, cos
 from datetime import datetime
 
 
-class Timer:
+def timer(fun):
 
-    def __init__(self, fun):
-        self.function = fun
-
-    def __call__(self, *args, **kwargs):
+    def wrapps(*args, **kwargs):
         start_time = datetime.now()
-        result = self.function(*args, **kwargs)
+        result = fun(*args, **kwargs)
         end_time = datetime.now()
         print(f" Время начала работы функции: {start_time}\n Время окончания работы функции: {end_time}\n"
               f" Общее время работы функции: {end_time - start_time}\n")
         return result
+    return wrapps
 
 
-@Timer
+@timer
 def calculate_mathematical_expression(number_x: int, number_y: int) -> float:
     return (cos(number_x) * sin(number_y))**sin(number_x + number_y)
 
