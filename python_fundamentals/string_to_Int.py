c7a153fc56210226febb5018127c50f43f423bf0
VeamVeat <vv1629728@gmail.com>
2021-10-18 12:56:25 +0300

    feat: fix naming and fix for loop

--- python_fundamentals/string_to_Int.py
+++ python_fundamentals/string_to_Int.py
@@ -1,27 +1,33 @@
 import re
 
 
-def str_to_int(user_string: str) -> int:
+def str_to_int(user_line: str) -> int:
+    """
+    :param user_line: строка пользователя
+    :return: '2345' -> 2000 + 300 + 40 + 5 -> 2345
+    """
 
     num_format = re.compile(r'^\-?[1-9][0-9]*\.?[0-9]*')
-    is_number = re.match(num_format, user_string)
+    is_number = re.match(num_format, user_line)
 
     if is_number:
-        str_number = [f'{i}' for i in range(10)]
+
+        str_number = {'0': 0, '1': 1, '2': 2,
+                      '3': 3, '4': 4, '5': 5,
+                      '6': 6, '7': 7, '8': 8, '9': 9}
+
+        last_item_in_line = len(user_line) - 1
         result_list = []
 
-        for user_index_in_string, user_symbol_in_string in enumerate(user_string):
-            for index_in_str_number, symbol_in_str_number in enumerate(str_number):
-
-                if symbol_in_str_number == user_symbol_in_string and user_index_in_string == len(user_string) - 1:
-                    result_list.append(index_in_str_number)
-
-                elif symbol_in_str_number == user_symbol_in_string:
-                    result_list.append(index_in_str_number * 10 ** (len(user_string) - 1 - user_index_in_string))
+        for id_in_user_string, symbol_in_user_string in enumerate(user_line):
+            if id_in_user_string == last_item_in_line:
+                result_list.append(str_number[symbol_in_user_string])
+            else:
+                result_list.append(str_number[symbol_in_user_string] * 10 ** (len(user_line) - 1 - id_in_user_string))
 
     else:
-        raise Exception("Строка " + user_string + " не является валидной!")
-    return sum(result_list) if user_string[0] != '-' else sum(result_list) * -1
+        raise Exception("Строка " + user_line + " не является валидной!")
+    return sum(result_list) if user_line[0] != '-' else sum(result_list) * -1
 
 
 string_input = input()
