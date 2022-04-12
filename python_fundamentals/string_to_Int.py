e48a3e16f808d58496a5951977af717df3a79ef3
Vadim <vadim.vasiliev.pro@gmail.com>
2022-04-12 03:42:49 +0300

    feat: changed method str_to_int

--- python_fundamentals/string_to_Int.py
+++ python_fundamentals/string_to_Int.py
@@ -1,34 +1,41 @@
 import re
 
 
-def str_to_int(user_line: str) -> int:
+def str_to_int(user_string: str) -> int:
     """
-    :param user_line: строка пользователя
+    :param user_string: user string
     :return: '2345' -> 2000 + 300 + 40 + 5 -> 2345
     """
 
+    result_int = 0
+    start_range = 0
+    is_minus = user_string[0] == '-'
+
     num_format = re.compile(r'^\-?[1-9][0-9]*\.?[0-9]*')
-    is_number = re.match(num_format, user_line)
+    is_number = re.match(num_format, user_string)
 
-    if is_number:
-
-        str_numbers = {'0': 0, '1': 1, '2': 2,
-                       '3': 3, '4': 4, '5': 5,
-                       '6': 6, '7': 7, '8': 8, '9': 9}
-
-        last_item_in_line = len(user_line) - 1
-        result_list = []
-
-        for id_in_user_string, symbol_in_user_string in enumerate(user_line):
-            if id_in_user_string == last_item_in_line:
-                result_list.append(str_numbers[symbol_in_user_string])
-            else:
-
-                result_list.append(str_numbers[symbol_in_user_string] * 10 ** (last_item_in_line - id_in_user_string))
-
+    if not is_number:
+        raise Exception("Строка " + user_string + " не является валидной!")
     else:
-        raise Exception("Строка " + user_line + " не является валидной!")
-    return sum(result_list) if user_line[0] != '-' else sum(result_list) * -1
+        if is_minus:
+            start_range = 1
+
+        number_dict = {
+            '1': 1, '2': 2,
+            '3': 3, '4': 4,
+            '5': 5, '6': 6,
+            '7': 7, '8': 8,
+            '9': 9, '0': 0,
+        }
+
+        len_user_string = len(user_string)
+
+        for index_user_string in range(start_range, len_user_string - 1):
+            number_with_dict = number_dict[user_string[index_user_string]]
+            result_int += number_with_dict * (10 ** (len_user_string - 1 - index_user_string))
+        result_int += number_dict[user_string[-1]]
+
+        return result_int if not is_minus else result_int * -1
 
 
 string_input = input()
