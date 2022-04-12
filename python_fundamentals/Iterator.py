d80d776a657bc2db995a53796b760d1e442f990c
Vadim <vadim.vasiliev.pro@gmail.com>
2022-04-12 03:32:25 +0300

    feat: changed task iterator

--- python_fundamentals/Iterator.py
+++ python_fundamentals/Iterator.py
@@ -1,17 +1,16 @@
 
-
 class MyIterator:
     def __init__(self, initial_number):
         self.number_to_square = initial_number
-        self.index = 0
+        self.start = 0
 
     def __next__(self):
-        if self.index <= len(self.number_to_square) - 1:
-            i = self.index
-            self.index += 1
-            return self.number_to_square[i]
-        else:
+        if self.start == len(self.number_to_square):
             raise StopIteration
+        else:
+            next_element = self.number_to_square[self.start]
+            self.start += 1
+            return next_element
 
     def __iter__(self):
         return self
