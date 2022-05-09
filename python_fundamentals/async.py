a371fbc55b39292a35de55540115ea0891daa6f0
Vadim <vadim.vasiliev.pro@gmail.com>
2022-05-09 18:06:20 +0300

    feat: changed async task

--- python_fundamentals/async.py
+++ python_fundamentals/async.py
@@ -1,39 +1,46 @@
-import requests as r
+import os
+import requests
 import asyncio
 
-
-async def save_image_cat(img_content_response, img_name):
-    print("Сохранение изображения")
-
-    with open(f"Cats/{img_name}", "wb") as file:
-        file.write((await img_content_response).content)
+os.makedirs(os.path.dirname('Cats/'), exist_ok=True)
 
 
-async def get_image_cat(img_future):
-    img_future = await img_future
-    print(img_future, "Получение изображения")
-
-    if img_future.status_code == 200:
-        img_url: str = img_future.json()[0]["url"]
-        img_name = img_url.split('/')[-1]
-
-        await save_image_cat(loop.run_in_executor(None, r.get, img_url), img_name)
+async def download_img():
+    image_json = loop.run_in_executor(None, requests.get, 'https://api.thecatapi.com/v1/images/search')
+    url_img = (await image_json).json()[0]['url']
+    print(f'Получение {url_img}')
+    return url_img
 
 
-async def main(count_task: int):
+async def save_img(url_img, img_name, img_extension):
+    response = loop.run_in_executor(None, requests.get, url_img)
+    img = (await response).content
+    print(f'Сохранение {img_name}.{img_extension}')
+    with open(f"Cats/{img_name}.{img_extension}", "wb+") as f:
+        f.write(img)
 
-    info_img_response = f'https://api.thecatapi.com/v1/images/search'
 
-    task = [loop.create_task(get_image_cat(loop.run_in_executor(None, r.get, info_img_response)))
-            for _ in range(count_task)]
+async def get_cat_img():
+    url_img = await download_img()
 
-    await asyncio.gather(*task)
+    img = url_img.split('/')[-1]
+    img_name = img.split('.')[0]
+    img_extension = img.split('.')[-1]
+
+    print(url_img)
+    await save_img(url_img, img_name, img_extension)
+
+
+async def create_tasks_func(count_img: int):
+    tasks = [asyncio.create_task(get_cat_img()) for _ in range(count_img)]
+    await asyncio.wait(tasks)
 
 
 if __name__ == "__main__":
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(main(5))
-
+        loop.run_until_complete(create_tasks_func(10))
+        loop.close()
     except:
         pass
+
